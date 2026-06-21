d9988b5f45df86742ce560cb4d3053e9dde5145c
vyrgyn33-cpu <vyrgyn33@gmail.com>
2026-06-21 02:11:06 -0300

    Add files via upload

---
+++ dulceMania/vista/js/main.js
@@ -0,0 +1,54 @@
+// =====================
+// SCROLL SUAVE
+// =====================
+
+document.querySelectorAll('a[href^="#"]').forEach(enlace => {
+
+    enlace.addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        const destino = document.querySelector(
+            this.getAttribute('href')
+        );
+
+        if(destino){
+
+            destino.scrollIntoView({
+                behavior:'smooth'
+            });
+
+        }
+
+    });
+
+});
+
+
+// =====================
+// MENSAJE DE CARGA
+// =====================
+
+window.addEventListener("load", ()=>{
+
+    console.log("Dulce Manía cargado correctamente");
+
+});
+
+
+// =====================
+// WHATSAPP
+// =====================
+
+function pedirHelado(sabor){
+
+    const numero = "541134223324";
+
+    const mensaje =
+        `Hola Dulce Manía, quiero pedir ${sabor}`;
+
+    window.open(
+        `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`
+    );
+
+}
